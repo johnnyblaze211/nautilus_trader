48a57b40f7773f054ec0ae1bdbf66beb24fae5f8
Dhairya Gupta <dhairyagupta.211@gmail.com>
2025-09-04 15:57:35 +0530

    Refactor tracemalloc_trade_ticks.py to use common utility
    - Reduce code duplication by using create_memory_test_functions
    - Maintain same functionality with cleaner, more maintainable code
    - Reduces file from 37 to 27 lines

--- tests/mem_leak_tests/tracemalloc_trade_ticks.py
+++ tests/mem_leak_tests/tracemalloc_trade_ticks.py
@@ -15,22 +15,12 @@
 # -------------------------------------------------------------------------------------------------
 
 from nautilus_trader.model.data import TradeTick
-from nautilus_trader.test_kit.rust.data_pyo3 import TestDataProviderPyo3
-from nautilus_trader.test_kit.stubs.data import TestDataStubs
-from tests.mem_leak_tests.conftest import snapshot_memory
+from tests.mem_leak_tests.test_utils import create_memory_test_functions
 
-
-@snapshot_memory(4000)
-def run_repr(*args, **kwargs):
-    trade = TestDataStubs.trade_tick()
-    repr(trade)
-
-
-@snapshot_memory(4000)
-def run_from_pyo3(*args, **kwargs):
-    pyo3_trade = TestDataProviderPyo3.trade_tick()
-    TradeTick.from_pyo3(pyo3_trade)
-
+# Create test functions using the utility
+run_repr, run_from_pyo3 = create_memory_test_functions(
+    "TradeTick", TradeTick, "trade_tick", "trade_tick"
+)
 
 if __name__ == "__main__":
     run_repr()

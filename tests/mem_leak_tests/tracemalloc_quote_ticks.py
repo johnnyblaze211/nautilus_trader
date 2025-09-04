218f4ede0baadc85d23cc04f2e736e2162dde4cf
Dhairya Gupta <dhairyagupta.211@gmail.com>
2025-09-04 15:57:03 +0530

    Refactor tracemalloc_quote_ticks.py to use common utility
    - Reduce code duplication by using create_memory_test_functions
    - Maintain same functionality with cleaner, more maintainable code
    - Reduces file from 37 to 27 lines

--- tests/mem_leak_tests/tracemalloc_quote_ticks.py
+++ tests/mem_leak_tests/tracemalloc_quote_ticks.py
@@ -15,22 +15,12 @@
 # -------------------------------------------------------------------------------------------------
 
 from nautilus_trader.model.data import QuoteTick
-from nautilus_trader.test_kit.rust.data_pyo3 import TestDataProviderPyo3
-from nautilus_trader.test_kit.stubs.data import TestDataStubs
-from tests.mem_leak_tests.conftest import snapshot_memory
+from tests.mem_leak_tests.test_utils import create_memory_test_functions
 
-
-@snapshot_memory(4000)
-def run_repr(*args, **kwargs):
-    quote = TestDataStubs.quote_tick()
-    repr(quote)
-
-
-@snapshot_memory(4000)
-def run_from_pyo3(*args, **kwargs):
-    pyo3_quote = TestDataProviderPyo3.quote_tick()
-    QuoteTick.from_pyo3(pyo3_quote)
-
+# Create test functions using the utility
+run_repr, run_from_pyo3 = create_memory_test_functions(
+    "QuoteTick", QuoteTick, "quote_tick", "quote_tick"
+)
 
 if __name__ == "__main__":
     run_repr()
